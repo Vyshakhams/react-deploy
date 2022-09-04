a1a208e72eba423cbcdd7a5ec67aa61487e99d63
vyshakhams <vyshakhams>
2022-09-04 11:33:29 +0530

    Added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import jsonData from "../MOCK_DATA.json";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log(jsonData);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
