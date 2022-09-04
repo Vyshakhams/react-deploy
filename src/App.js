fdcc5ed96b5c673aed7c67f25549c97af3dbd889
vyshakhams <vyshakhams>
2022-09-04 11:59:21 +0530

    tes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Jobs />} />
+        <Route path="/react-deploy" element={<Jobs />} />
       </Routes>
     </div>
   );
